1ea8976d528146da210773ca52e5b7ac47ef1eac
Mariusz <mario.m4@interia.pl>
2023-01-03 12:58:00 +0100

    React router integrated, and styles for button

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -17,7 +17,7 @@ export const Header = () => {
             <h1>
                 <strong>Ogłoszenia</strong>
             </h1>
-            <Btn text={'dodaj ogłoszenie'}/>
+            <Btn to="/add" text={'dodaj ogłoszenie'}/>
             <form className="search" onSubmit={setSearchFromLocalState}>
                 <input type="text" value={inputVal} onChange={e => setInputVal(e.target.value)}/> <Btn text={'Szukaj'}/>
             </form>
